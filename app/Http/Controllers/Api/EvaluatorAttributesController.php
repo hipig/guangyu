58bcd9d1bcf86a4f6ed60558bab176f228c2a1da
hipig <205270006@qq.com>
2022-07-09 21:58:36 +0800

    修改估价属性排序

--- app/Http/Controllers/Api/EvaluatorAttributesController.php
+++ app/Http/Controllers/Api/EvaluatorAttributesController.php
@@ -15,6 +15,15 @@ class EvaluatorAttributesController extends Controller
             ->rank()
             ->enable()
             ->get();
+        $attributes->each(function ($attribute) {
+           if (in_array($attribute->key, ['gift_bags', 'hot_items'])) {
+               $attribute->options = collect($attribute->options)
+                   ->map(function ($option) {
+                       $option['abbr'] = mb_substr(pinyin_abbr($option['label']), 0, 1);
+                       return $option;
+                   })->sortBy('abbr')->values()->toArray();
+           }
+        });
 
         return EvaluatorAttributeResource::collection($attributes);
     }
